b8504c506a491dbddbdf7ca904cb1327e69d5a54
John Keyes <john@johnkeyes.com>
2012-12-24 15:16:44 -0500

    Started an SBUsersViewController.

---
+++ SoundBite/SBUsersViewController.h
@@ -0,0 +1,13 @@
+//
+//  SBUsersViewController.h
+//  SoundBite
+//
+//  Created by John Keyes on 12/24/12.
+//  Copyright (c) 2012 John Keyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBUsersViewController : UITableViewController
+
+@end
