8c0437948ab85703ec4826ab0280e7d9bf78432d
John Keyes <john@new-host.home>
2013-01-15 20:32:22 -0500

    Really adding the icons files this time.

---
+++ SoundBite/ChannelTypes.h
@@ -0,0 +1,17 @@
+#define CHANNEL_TYPE_NONE		0
+#define CHANNEL_TYPE_VOICE		1
+#define CHANNEL_TYPE_EMAIL		2
+#define CHANNEL_TYPE_TEXT		3
+#define CHANNEL_TYPE_DIALER		4
+#define CHANNEL_TYPE_PREVIEW	5
+#define CHANNEL_TYPE_MANUAL		6
+#define CHANNEL_TYPE_WEB		7
+
+#define CHANNEL_ICON_NONE		@"22-skull-n-bones.png"
+#define CHANNEL_ICON_VOICE		@"66-microphone.png"
+#define CHANNEL_ICON_EMAIL		@"18-envelope.png"
+#define CHANNEL_ICON_TEXT		@"32-iphone.png"
+#define CHANNEL_ICON_DIALER		@"75-phone.png"
+#define CHANNEL_ICON_PREVIEW	@"06-magnify.png"
+#define CHANNEL_ICON_MANUAL		@"40-dialpad.png"
+#define CHANNEL_ICON_WEB		@"401-globe.png"
